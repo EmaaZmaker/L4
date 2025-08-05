c79388c7ef0c4a18bd35df62300138c83160f5b7
EmaaZmaker <emaazbinammar@gmail.com>
2025-08-05 10:37:40 +0400

    eu

---
+++ act3.py
@@ -0,0 +1,10 @@
+class points:
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        return (f"({self.x},{self.y})")
+obj1=points(6,8)
+print(obj1)
+
+        
